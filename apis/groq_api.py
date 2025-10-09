fa37c03e4d32b8d7500ffcf2da29a6152ed03132
unknown <damimelgarejosanchez@gmail.com>
2025-10-09 17:15:57 -0300

    Actualizado, ahora si el system rpomt del translucidor de audios

--- apis/groq_api.py
+++ apis/groq_api.py
@@ -68,7 +68,11 @@ def transcribe_voice_with_groq(file_path: str) -> Optional[str]:
             transcription = groq_client.audio.transcriptions.create(
                 file=(file_path, file),
                 model="whisper-large-v3-turbo",
-                prompt=system_prompt,
+                prompt='Eres un asistente que responde preguntas sobre bancos y sus tarifas mensuales en Argentina.' \
+                'Usa la información del dataset para responder con precisión, claridad y contexto actualizado.' \
+                'Interpreta imágenes y audios para extraer texto y responder según las reglas.' \
+                'Dataset de referencia:' \
+                '{json.dumps(bank_data, ensure_ascii=False, indent=2)}',
                 response_format="json",
                 language="es",
             )
